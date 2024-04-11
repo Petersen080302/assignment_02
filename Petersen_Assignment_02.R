c0e6e5bd2ca22fed1a0c413e933308ae976345df
Petersen080302 <166718749+Petersen080302@users.noreply.github.com>
2024-04-11 15:10:42 +0200

    Add files via upload

---
+++ Petersen_Assignment_02.R
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Assignment 2 by Paul Petersen
+
+# I will be using the V_Dem_CY_Full_Others_v14 dataset
+
+library(tidyverse)
+
+library(readr)
+V_Dem_CY_Full_Others_v14 <- read_csv("Documents/Semester 4 Politik/Ü Methoden VR (Data Collection with R)/Midterm Assignment/Midterm_Petersen/V-Dem-CY-Full+Others-v14.csv")
+View(V_Dem_CY_Full_Others_v14)
+
+# I will show the development of Political Corruption Index in Mexico over time
+
+ggplot(data = V_Dem_CY_Full_Others_v14 %>% filter(country_name == "Mexico"),
+aes(x = year, y = v2x_corr)) +
+  geom_point() +
+  scale_x_log10() +
+  geom_smooth(method = "lm") +
+  labs(x = "year", 
+       y = "Political Corruption Index",
+       color = "Continent",
+       title = "",
+       subtitle = "Year / Index Development",
+       caption = "V_Dem_CY_Full_Others_v14") 
